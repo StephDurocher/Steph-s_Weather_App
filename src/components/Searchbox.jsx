101525ad2f470f5df9f86c23e95fb34759b682f9
Stephanie Durocher <sdurocher722@gmail.com>
2023-10-09 14:01:25 -0400

    added state to search button

--- src/components/Searchbox.jsx
+++ src/components/Searchbox.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Searchbox(props) {
   const { onSubmit } = props;
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  const handleOnChange = (e) => {
+    const value = e.target.value;
+    if (value.length > 0) {
+      setIsDisabled(false);
+    } else if (value.length === 0) {
+      console.log("hello");
+      setIsDisabled(true);
+    }
+  };
 
   return (
     <form onSubmit={onSubmit}>
       <div className="mb-3">
         <label>
-          <input type="text" placeholder="search city..." />
+          <input
+            type="text"
+            placeholder="search city..."
+            name="city"
+            onChange={handleOnChange}
+          />
         </label>
-        <input type="submit" value="Search" />
+        <input type="submit" value="Search" disabled={isDisabled} />
       </div>
     </form>
   );
